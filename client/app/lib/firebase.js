d3ca1945bd9c03612ae5b44d3499ae6758249cd7
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-09-15 23:30:41 +0530

    fix

--- client/app/lib/firebase.js
+++ client/app/lib/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_API_KEY,
-  authDomain: NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: NEXT_PUBLIC_APP_ID,
-  measurementId: NEXT_PUBLIC_MEASUREMENT_ID
+  apiKey: "AIzaSyBlqlCxGjIS16NlpEUhD3WS97FajZ8188I",
+  authDomain: "tunescape-c29b2.firebaseapp.com",
+  projectId: "tunescape-c29b2",
+  storageBucket: "tunescape-c29b2.appspot.com",
+  messagingSenderId: "37327276361",
+  appId: "1:37327276361:web:b0e4f963d5e117c829f298",
+  measurementId: "G-WH67XGNXS0"
 };
 
 // Initialize Firebase
