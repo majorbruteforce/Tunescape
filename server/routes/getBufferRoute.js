b273f11ac26b87916c5c8301bd40f92e4e3ce440
majorbruteforce <saswat3002@gmail.com>
2024-01-24 20:55:16 +0530

    getBufferRoute POST to GET

--- server/routes/getBufferRoute.js
+++ server/routes/getBufferRoute.js
@@ -3,7 +3,7 @@ const Route = express.Router();
 const {getBuffer}= require("../database/controllers/musicDB.js");
 
 
-Route.post('/',async(req,res)=>{
+Route.get('/',async(req,res)=>{
     console.log("request from client!")
     const songs= await getBuffer(parseInt(req.query.number));
     res.json(songs);
